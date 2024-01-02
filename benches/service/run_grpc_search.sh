404d89cbfab9feecd1666eef79664e2bca3ac832
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2024-01-02 15:06:01 +0000

    add commit tag thing

--- benches/service/run_grpc_search.sh
+++ benches/service/run_grpc_search.sh
@@ -3,6 +3,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo "$(git show -s --format=%H)"
 
 docker run --network=host \
   -v "${PWD}"/lib/api/src/grpc/proto:/proto \
